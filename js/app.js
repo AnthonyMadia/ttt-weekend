dd670022bfb2ef02a22dd273ce26200f5334636c
Anthony Madia <aamadia24@gmail.com>
2022-02-04 14:07:17 -0500

    Initialize variables, Cached Element Variables, and Event Listener

--- js/app.js
+++ js/app.js
@@ -3,16 +3,26 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let squares, turn, winner 
+let turnCount = 1
+let isWinner = false
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+let gameState = document.getElementById('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+document.querySelector('board').addEventListener('click', onClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
+function init() {
+
+}
+
+function onClick() {
+
+}
