9bb466d9c8ddf56cc141a4a6edf949fc57d6ea24
Anthony Madia <aamadia24@gmail.com>
2022-02-05 15:45:30 -0500

    Add: constant, init

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
+const winArr = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 // 1) Define the required variables used to track the state of the game:
@@ -10,54 +20,42 @@
 	  // a player that won
 	  // a tie has occured
 	  // or a game that is still in play.
-let squares, turn, winner, player
+let squares, turn, winner, player, isWinner, playerName
 
 
 /*------------------------ Cached Element References ------------------------*/
 // 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
-let gameUpdate = document.querySelector('#message') //game status
+
 let restartBtn = document.querySelector('#restart-btn')
 let gameBoard = document.querySelector('#board')
 
 // 2.1) Store the 9 elements that represent the squares on the page.
 	  // You may want to give each square a class name in your HTML to make this easier!
-
-
+// 2.2) Store the element that displays the game status on the page
+let message = document.querySelector('#message') 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-board.addEventListener('click', onClick)
 restartBtn.addEventListener('click', init)
+squares.addEventListener('click', onClick)
+gameBoard.addEventListener("click", playyerTurn)
+
 
 
 /*-------------------------------- Functions --------------------------------*/
 init();
 
 function init() {
-  gameUpdate.textContent = "It is X's turn!"
-  // restartBtn.setAttribute('hidden', true)
-  
-}
-
-function createGame() {
-  board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null']
+  message.textContent = "It is X's turn!"
+  restartBtn.setAttribute('hidden', true)
+  squares = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
   turn = 1
   player = -1
   winner = null
-}
-function render() {
-
-}
-function onClick(evt) {
-  
-  
-}
-
-function checkWin() {
-  if (!board === null) {
-    gameUpdate.textContent = "Tie Game"
-  }
+  isWinner = false
+  render()
+  checkWin()
 }
 
