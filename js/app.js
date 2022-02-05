8628ac9ffb698ff466a296f762d266c917b708c2
Anthony Madia <aamadia24@gmail.com>
2022-02-05 16:28:03 -0500

    Add: handleClick function

--- js/app.js
+++ js/app.js
@@ -20,42 +20,52 @@ const winArr = [
 	  // a player that won
 	  // a tie has occured
 	  // or a game that is still in play.
-let squares, turn, winner, player, isWinner, playerName
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
-
-let restartBtn = document.querySelector('#restart-btn')
-let gameBoard = document.querySelector('#board')
-
-// 2.1) Store the 9 elements that represent the squares on the page.
-	  // You may want to give each square a class name in your HTML to make this easier!
-// 2.2) Store the element that displays the game status on the page
-let message = document.querySelector('#message') 
+const restartBtn = document.getElementById('reset')
+const squares = document.querySelectorAll('section > div')
+const message = document.getElementById('message') 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-restartBtn.addEventListener('click', init)
-squares.addEventListener('click', onClick)
-gameBoard.addEventListener("click", playyerTurn)
-
+squares.forEach((square) => {square.addEventListener("click", handleClick)})
+resetBtn.addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
 init();
 
 function init() {
-  message.textContent = "It is X's turn!"
-  restartBtn.setAttribute('hidden', true)
-  squares = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
+  // message.textContent = "It is X's turn!"
+  // restartBtn.setAttribute('hidden', true)
+  // clear board
+  board = [null, null, null, null, null, null, null, null, null] 
+  // set turn for Player X which equals 1
   turn = 1
-  player = -1
+  // initialize winner variable
   winner = null
-  isWinner = false
   render()
-  checkWin()
 }
 
+function render() {
+
+}
+
+function handleClick(event) {
+  // Know what index the mouse clicks on
+  // get index from id and make it into a number to do computation on it
+  let sqIdx = parseInt(event.target.id.replace('sq', ''))
+  // check to see if board is taken, if it is, return
+  if (board[sqIdx] || winner) {
+    return
+  }
+  // update board 
+  board[sqIdx] = turn
+  turn *= -1
+  // state updated, so call render
+  render()
+}
